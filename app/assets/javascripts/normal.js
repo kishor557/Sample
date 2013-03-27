e1c927ac429e6d1f932170fd2810095c3a330fd8
kishore <re16@re16-desktop>
2013-03-27 13:05:39 +0530

    more changes

---
+++ app/assets/javascripts/normal.js
@@ -0,0 +1,13 @@
+$('.popup').click(function(e) {
+  console.log("e");
+  popupCenter($(this).attr("href"), $(this).attr("data-width"), $(this).attr("data-height"), "authPopup");
+  e.stopPropagation(); return false;
+});
+
+
+function popupCenter(url, width, height, name) {
+  console.log("f");
+  var left = (screen.width/2)-(width/2);
+  var top = (screen.height/2)-(height/2);
+  return window.open(url, name, "menubar=no,toolbar=no,status=no,width="+width+",height="+height+",toolbar=no,left="+left+",top="+top);
+}
